b9a5c26dd244c07835644d4e7efe12fac8ca6068
Marian13O <142900026+Marian13O@users.noreply.github.com>
2024-01-10 14:06:27 +0200

    edit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-import basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/+esm";
+import basiclightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/+esm";
 const modal = basicLightbox.create(`
     <img>
 `),
